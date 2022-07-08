117a448b077352b948f0cd18c725030055db9470
Vladyslav Hurzhyi <soulline0@gmail.com>
2022-07-08 15:11:37 +0200

    link fix

--- js/modal.js
+++ js/modal.js
@@ -14,3 +14,17 @@
         refs.body.classList.toggle('no-scroll')
     }
 })()
+;(() => {
+    const refs = {
+        openMenubtn: document.querySelector('[data-menu-open]'),
+
+        menu: document.querySelector('[data-menu]'),
+        body: document.querySelector('body'),
+    }
+
+    refs.openMenubtn.addEventListener('click', toggleModal)
+
+    function toggleModal() {
+        refs.body.classList.toggle('no-scroll')
+    }
+})()
